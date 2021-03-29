301b3a278ab9c1fb6303c6936638daa28469cf43
Tania <40333656+tanialx@users.noreply.github.com>
2021-03-29 15:28:41 +0700

    Rotate the cube on mouse drags (#1)
    * Add renderer.js + disable auto rotation
    * Rotate cube based on mouse events (ref: https://www.tutorialspoint.com/webgl/webgl_interactive_cube.htm)
    * Replace manual matrix calculation for rotation with mat4 api rotateX, rotateY

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let cubeRotation = 0.0;
 let roobix = new Roobix(3);
+let renderer;
 
 main();
 
@@ -15,11 +15,6 @@ function main() {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
-
-    // Set clear color to black, fully opaque
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
 
     // Create and link the shader program to WebGL context
     const shaderProgram = initShaderProgram(gl);
@@ -45,214 +40,45 @@ function main() {
         },
     };
 
-    const buffers = initBuffers(gl);
-
-    var then = 0;
-
-    // Draw the scene repeatedly
-    function render(now) {
-        now *= 0.001;  // convert to seconds
-        const deltaTime = now - then;
-        then = now;
-
-        drawScene(gl, programInfo, buffers, deltaTime);
-
-        requestAnimationFrame(render);
+    const roobixData = {
+        v: roobix.vertices,
+        fColors: roobix.colors,
+        vCount: roobix.vertexCount
     }
-    requestAnimationFrame(render);
-}
+    renderer = new Renderer(gl, programInfo, roobixData);
 
-function initBuffers(gl) {
+    const mouseControl = new MouseControl(canvas.width, canvas.height);
+    canvas.addEventListener("mousedown", function (e) {
+        mouseControl.mouseDown(e);
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        mouseControl.mouseUp(e);
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        mouseControl.mouseOut(e);
+    }, false);
+    canvas.addEventListener("mousemove", function (e) {
+        mouseControl.mouseMove(e);
+    }, false);
 
-    // Create an array of positions for the cube.
+    let animate = function () {
 
-    const positions = roobix.vertices;
-
-    // Create a buffer for the cube's positions.
-
-    const positionBuffer = gl.createBuffer();
-
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    /**
-     * Colors
-     */
-
-    const faceColors = roobix.colors;
-    // Convert the array of colors into a table for all the vertices.
-    var colors = [];
-    for (var j = 0; j < faceColors.length; ++j) {
-        const c = faceColors[j];
-        // Repeat each color four times for the four vertices of the face
-        colors = colors.concat(c, c, c, c);
-    }
-
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
-
-    /*
-    const indices = [
-        0, 1, 2, 0, 2, 3,           // front
-        4, 5, 6, 4, 6, 7,           // back
-        8, 9, 10, 8, 10, 11,        // top
-        12, 13, 14, 12, 14, 15,     // bottom
-        16, 17, 18, 16, 18, 19,     // right
-        20, 21, 22, 20, 22, 23,     // left
-    ];
-    */
-    var indices = [];
-    const templ_arr = [0, 1, 2, 0, 2, 3];
-    let counter = -4;
-    for (var i = 0; i <= positions.length / 2; ++i) {
-        const imod6 = i % 6;
-        if (imod6 == 0) {
-            counter += 4;
+        if (!mouseControl.drag) {
+            mouseControl.dX *= MouseControl.AMORTIZATION;
+            mouseControl.dY *= MouseControl.AMORTIZATION;
+            mouseControl.total_dragged_x += mouseControl.dX;
+            mouseControl.total_dragged_y += mouseControl.dY;
         }
-        indices.push(counter + templ_arr[imod6]);
+
+        let modelMat = mat4.create();
+        mat4.rotateX(modelMat, modelMat, mouseControl.total_dragged_y);
+        mat4.rotateY(modelMat, modelMat, mouseControl.total_dragged_x);
+
+        renderer.clear();
+        renderer.rotationMat = modelMat;
+        renderer.render();
+
+        window.requestAnimationFrame(animate);
     }
-
-    // Now send the element array to GL
-
-    // Element array
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
-    return {
-        position: positionBuffer,
-        color: colorBuffer,
-        indices: indexBuffer
-    };
-}
-
-function drawScene(gl, programInfo, buffers, deltaTime) {
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-    // Clear the canvas before we start drawing on it.
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-
-    // note: glmatrix.js always has the first argument
-    // as the destination to receive the result.
-    mat4.perspective(projectionMatrix,
-        fieldOfView,
-        aspect,
-        zNear,
-        zFar);
-
-    // Set the drawing position to the "identity" point, which is
-    // the center of the scene.
-    const modelViewMatrix = mat4.create();
-
-    // Now move the drawing position a bit to where we want to
-    // start drawing the cube.
-
-    mat4.translate(modelViewMatrix,     // destination matrix
-        modelViewMatrix,     // matrix to translate
-        [-0.0, 0.0, -6.0]);  // amount to translate
-
-    mat4.rotate(modelViewMatrix,  // destination matrix
-        modelViewMatrix,  // matrix to rotate
-        cubeRotation * .7,   // amount to rotate in radians
-        [0.5, 0.5, 0.5]);       // axis to rotate around
-
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute.
-    {
-        const numComponents = 3;  // pull out 3 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-        // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexPosition);
-    }
-
-    // Tell WebGL how to pull out the colors from the color buffer
-    // into the vertexColor attribute.
-    {
-        const numComponents = 4;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexColor,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexColor);
-    }
-
-    // Tell WebGL which indices to use to index the vertices
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-
-    // Tell WebGL to use our program when drawing
-
-    gl.useProgram(programInfo.program);
-
-    // Set the shader uniforms
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.projectionMatrix,
-        false,
-        projectionMatrix);
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.modelViewMatrix,
-        false,
-        modelViewMatrix);
-
-    {
-        const vertexCount = roobix.vertexCount * 3/2;
-        const type = gl.UNSIGNED_SHORT;
-        const offset = 0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-    }
-
-    // Update the rotation for the next draw
-
-    cubeRotation += deltaTime;
+    animate(0);
 }
---
+++ mouseControl.js
@@ -0,0 +1,38 @@
+class MouseControl {
+
+    static AMORTIZATION = .95;
+
+    constructor(w, h) {
+        this.drag = false;
+        this.old_x = 0;
+        this.old_y = 0;
+        this.dX = 0;
+        this.dY = 0;
+        this.w = w;
+        this.h = h;
+        this.total_dragged_x = 0;
+        this.total_dragged_y = 0;
+    }
+
+    mouseDown(e) {
+        this.drag = true;
+        this.old_x = e.pageX, this.old_y = e.pageY;
+        e.preventDefault();
+        return false;
+    }
+
+    mouseUp(e) {
+        this.drag = false;
+    };
+
+    mouseMove(e) {
+        if (!this.drag) return false;
+        this.dX = (e.pageX - this.old_x) * 2 * Math.PI / this.w;
+        this.dY = (e.pageY - this.old_y) * 2 * Math.PI / this.h;
+        this.total_dragged_x += this.dX;
+        this.total_dragged_y += this.dY;
+        this.old_x = e.pageX;
+        this.old_y = e.pageY;
+        e.preventDefault();
+    };
+}
